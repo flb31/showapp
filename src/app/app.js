919d1a98e98318585d44b98a6b263544bf25d40c
Fabian Luna <flb031@gmail.com>
2017-09-27 16:36:40 -0500

    Added route

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,24 @@
-var dependencies = [];
+var dependencies = [
+  'ngRoute'
+];
 
-angular.module('showApp', dependencies);
+angular.module('showApp', dependencies)
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+      .when('/', {
+        templateUrl: '../app/layouts/movie/movie.html'
+      })
+      .when('/movies', {
+        templateUrl: '../app/layouts/movie/movie.html'
+      })
+      .when('/series', {
+        templateUrl: '../app/layouts/serie/serie.html'
+      })
+      .when('/favorites', {
+        templateUrl: '../app/layouts/favorite/favorite.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
