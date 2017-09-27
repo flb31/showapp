6dd39599110dc8a75ca1ed766f1eb6cf192aac9e
Fabian Luna <flb031@gmail.com>
2017-09-27 17:16:54 -0500

    Url dynamic

--- src/app/components/showapp-header/header.controller.js
+++ src/app/components/showapp-header/header.controller.js
@@ -1,6 +1,30 @@
 angular.module('showApp').controller('HeaderController', [
-  'Title', function (Title) {
+  'Title', '$location', function (Title, $location) {
     const self = this;
     self.title = Title;
+    self.menu = [
+      { name: 'Movies', url:'/movies', class: '' },
+      { name: 'Series', url:'/series', class: '' },
+      { name: 'Favoritos', url:'/favorites', class: '' }
+    ];
+    const menuActive = 'menu__item-active';
+
+    const verifyMenuByUrl = () => {
+      const url = $location.url();
+      self.menu.forEach( (el) => {
+        if(url == el.url) {
+          el.class = menuActive;
+        }
+      });
+    };
+
+    self.activeMenuItem = (index) => {
+      self.menu.forEach( (el) => {
+        el.class = '';
+      });
+      self.menu[index].class = menuActive;
+    };
+    
+    verifyMenuByUrl();
   }]
 );
